f326fa373c7c862e48d45cdc52dc96200fef118e
Marek Choiński <marekchoinski17@gmail.com>
2019-12-20 17:27:22 +0100

    Dividing files for re-using in the future

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -12,144 +12,16 @@ import { useFirestoreConnect, useFirebase } from 'react-redux-firebase'
 import Spinner from 'react-bootstrap/Spinner';
 import SortBar from '../components/SortBar';
 import { subHours, getMilliseconds } from 'date-fns';
+import PostsList from '../components/PostsList';
 
 
 const Mainpage: React.FC = () => {
-
-
-
-
-    const sortMethod = useSelector((state: any) =>
-        state.posts.sortMethod
-    );
-
-
-    const favoritePosts = useSelector((state: any) =>
-        !state.firebase.auth.isEmpty ? state.firebase.profile.favoritePosts : []
-    );
-
-    // TODO make this as a selector returning tuple
-    let valueToOrderTo = "createdAt";
-
-    // let before = subHours(new Date(), 13);
-
-
-
-    const before = useMemo(
-        () => {
-            if (sortMethod == "top6") {
-                return subHours(new Date(), 6);
-            }
-            else if (sortMethod == "top12") {
-                return subHours(new Date(), 12);
-            }
-            else if (sortMethod == "top24") {
-                return subHours(new Date(), 24);
-            }
-            else return null;
-        },
-        [sortMethod]
-    );
-
-    if (sortMethod == 'newest') {
-        valueToOrderTo = "createdAt";
-        // before = null;
-    }
-    else {
-        valueToOrderTo = "likes";
-
-    }
-
-
-
-
-
-
-    const authors = useSelector((state: any) =>
-        state.firestore.ordered.authors ?
-            state.firestore.ordered.authors.reduce((hash: any, author: any) => {
-                hash[author.id] = {
-                    ...author
-                };
-                return hash;
-            }, {})
-            : null
-    );
-
-    const posts = useSelector((state: any) =>
-        authors ?
-            state.firestore.ordered.posts.map(
-                (post: any) =>
-                    ({
-                        ...post,
-                        author: authors[post.authorId],
-                        isFavorite: favoritePosts ? favoritePosts.includes(post.id) : false,
-                    })
-            )
-            : state.firestore.ordered.posts
-    );
-
-    //console.log("posts", posts);
-
-    const authorsQueries = posts ? posts.map((post: any) => ({
-        collection: 'users',
-        doc: post.authorId,
-        storeAs: 'authors'
-    })) : [];
-
-    useFirestoreConnect([
-        {
-            collection: 'posts',
-            where: before ? [
-                ['createdAt', '>', before]
-            ] : null,
-            orderBy: !before ? [
-                valueToOrderTo,
-                'desc'
-            ] : null,
-        },
-        ...authorsQueries,
-    ]);
-
-    if (before) {
-        posts.sort((a: any, b: any) => (b.likes - a.likes));
-    }
-
-    const postList = (authors) ? posts.map((post: any) =>
-
-        <PostCard
-            author={post.author.username}
-            authorId={post.author.id}
-            content={post.content}
-            authorProfilePicture={post.author.profilePicPath}
-            key={post.id}
-            id={post.id}
-            date={post.createdAt.seconds}
-            likedBy={post.likedBy}
-            favorite={post.isFavorite}
-        />
-    ) : <Spinner
-        animation="grow"
-        style={{
-            marginTop: "30vh",
-            marginLeft: "50vw",
-
-        }}
-        role="status"
-    >
-            <span className="sr-only">
-                Loading...
-    </span>
-        </Spinner>;
-
-
 
     return (
         <>
             <AddPostForm />
             <SortBar />
-            {postList}
-
+            <PostsList />
         </>
     );
 }
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import PostCard from '../components/PostCard';
 import { useFirestoreConnect } from 'react-redux-firebase'
@@ -8,7 +8,8 @@ import { useSelector } from 'react-redux';
 import Spinner from 'react-bootstrap/Spinner';
 import { Redirect } from 'react-router';
 import DefaultAvatar from '../assets/images/defaultAvatar.png';
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, subHours } from 'date-fns';
+import Nav from 'react-bootstrap/Nav';
 
 type Props = {
     match: {
@@ -23,42 +24,149 @@ const ProfilePage: React.FC<Props> = (props) => {
 
     //TODO redundancy with mainpage
 
-    // const authors = useSelector((state: any) =>
-    //     state.firestore.ordered.authors ?
-    //         state.firestore.ordered.authors.reduce((hash: any, author: any) => {
-    //             hash[author.id] = {
-    //                 ...author
-    //             };
-    //             return hash;
-    //         }, {})
-    //         : null
-    // );
 
-    // const posts = useSelector((state: any) =>
-    //     authors ?
-    //         state.firestore.ordered.posts.map(
-    //             (post: any) =>
-    //                 ({
-    //                     ...post,
-    //                     author: authors[post.authorId],
-    //                 })
-    //         )
-    //         : state.firestore.ordered.posts
-    // );
 
-    // const authorsQueries = posts ? posts.map((post: any) => ({
-    //     collection: 'users',
-    //     doc: post.authorId,
-    //     storeAs: 'authors'
-    // })) : [];
 
-    // useFirestoreConnect([
-    //     {
-    //         collection: 'posts',
-    //         doc: props.match.params.id,
-    //     },
-    //     ...authorsQueries,
-    // ])
+
+
+
+
+    const sortMethod = useSelector((state: any) =>
+        state.posts.sortMethod
+    );
+
+
+    const favoritePosts = useSelector((state: any) =>
+        !state.firebase.auth.isEmpty ? state.firebase.profile.favoritePosts : []
+    );
+
+    // TODO make this as a selector returning tuple
+    let valueToOrderTo = "createdAt";
+
+    // let before = subHours(new Date(), 13);
+
+
+
+    const before = useMemo(
+        () => {
+            if (sortMethod == "top6") {
+                return subHours(new Date(), 6);
+            }
+            else if (sortMethod == "top12") {
+                return subHours(new Date(), 12);
+            }
+            else if (sortMethod == "top24") {
+                return subHours(new Date(), 24);
+            }
+            else return null;
+        },
+        [sortMethod]
+    );
+
+    if (sortMethod == 'newest') {
+        valueToOrderTo = "createdAt";
+        // before = null;
+    }
+    else {
+        valueToOrderTo = "likes";
+
+    }
+
+
+
+
+
+
+    const authors = useSelector((state: any) =>
+        state.firestore.ordered.authors ?
+            state.firestore.ordered.authors.reduce((hash: any, author: any) => {
+                hash[author.id] = {
+                    ...author
+                };
+                return hash;
+            }, {})
+            : null
+    );
+
+    const posts = useSelector((state: any) =>
+        authors ?
+            state.firestore.ordered.posts.map(
+                (post: any) =>
+                    ({
+                        ...post,
+                        author: authors[post.authorId],
+                        isFavorite: favoritePosts ? favoritePosts.includes(post.id) : false,
+                    })
+            )
+            : state.firestore.ordered.posts
+    );
+
+    //console.log("posts", posts);
+
+    const authorsQueries = posts ? posts.map((post: any) => ({
+        collection: 'users',
+        doc: post.authorId,
+        storeAs: 'authors'
+    })) : [];
+
+    useFirestoreConnect([
+        {
+            collection: 'posts',
+            where: before ? [
+                ['createdAt', '>', before]
+            ] : null,
+            orderBy: !before ? [
+                valueToOrderTo,
+                'desc'
+            ] : null,
+        },
+        ...authorsQueries,
+    ]);
+
+    if (before) {
+        posts.sort((a: any, b: any) => (b.likes - a.likes));
+    }
+
+    const postList = (authors) ? posts.map((post: any) =>
+
+        <PostCard
+            author={post.author.username}
+            authorId={post.author.id}
+            content={post.content}
+            authorProfilePicture={post.author.profilePicPath}
+            key={post.id}
+            id={post.id}
+            date={post.createdAt.seconds}
+            likedBy={post.likedBy}
+            favorite={post.isFavorite}
+        />
+    ) : <Spinner
+        animation="grow"
+        style={{
+            marginTop: "30vh",
+            marginLeft: "50vw",
+
+        }}
+        role="status"
+    >
+            <span className="sr-only">
+                Loading...
+</span>
+        </Spinner>;
+
+
+
+
+
+
+
+
+
+
+
+    //TODO redundancy with mainpage
+
+
 
     useFirestoreConnect([
         {
@@ -99,34 +207,57 @@ const ProfilePage: React.FC<Props> = (props) => {
 
         !isRequestingProfileFromPage ?
             profile ?
-                <span>
-                    {profile.username} <br />
-                    <img
-                        width={64}
-                        height={64}
-                        className="mr-3"
-                        src={profile.profilePicPath || DefaultAvatar}
-                        // alt="placeholder"
+                <>
+
+                    <span
                         style={{
-                            borderRadius: "5px",
+                            margin: "100px auto 0 auto",
+                            width: "900px",
+                            display: "block"
                         }}
-                    /><br />
-                    {formatDistanceToNow(profile.createdAt.seconds * 1000, { addSuffix: true })}
-                    <br />
-                </span>
-                // posts[0] && authors ?
-                // <PostCard
-                //     author={posts[0].author.username}
-                //     content={posts[0].content}
-                //     authorProfilePicture={posts[0].author.profilePicPath}
-                //     key={posts[0].id}
-                //     date={posts[0].createdAt.seconds}
-                //     id={posts[0].id}
-                //     likedBy={posts[0].likedBy}
-                //     favorite={true} //TODO
-                // />
-                : null
-            //<Redirect to="/404" />
+                    >
+                        {profile.username} <br />
+                        <img
+                            width={64}
+                            height={64}
+                            className="mr-3"
+                            src={profile.profilePicPath || DefaultAvatar}
+                            // alt="placeholder"
+                            style={{
+                                borderRadius: "5px",
+                            }}
+                        /><br />
+                        {formatDistanceToNow(profile.createdAt.seconds * 1000, { addSuffix: true })}
+                        <br />
+                    </span>
+
+                    <Nav justify variant="tabs" defaultActiveKey="/home"
+                        style={{
+                            margin: "50px auto 0 auto",
+                            width: "900px",
+                        }}
+                    >
+                        <Nav.Item>
+                            <Nav.Link active>Najlepsze</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Najnowsze</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Ulubione</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Polubione</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>Obserwowani</Nav.Link>
+                        </Nav.Item>
+
+                    </Nav>
+
+                    {postList}
+                </>
+                : null//<Redirect to="/404" />
 
             : <Spinner
                 animation="grow"
