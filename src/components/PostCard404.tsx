0a0ff0e5ca6677f58aa6baacc5febbdcd47cc080
Marek Choiński <marekchoinski17@gmail.com>
2020-02-18 14:15:53 +0100

    Added components for 404 PostCard

---
+++ src/components/PostCard404.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+
+
+
+
+const PostCard404: React.FC = () => {
+
+
+
+    return (
+        <span
+            className="postcard404"
+        >
+            Niestety nie ma żadnych postów do wyświetlenia ;'(
+        </span>
+    );
+
+
+};
+
+export default PostCard404;
