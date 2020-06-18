a2edd057b223d604b2bbaf2766bb842f44370c56
Marek Choiński <marekchoinski17@gmail.com>
2020-06-19 00:16:24 +0200

    Updates

---
+++ precache-manifest.4544d1610c1a31413b94f3f30f58b8cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf73ba7f0edd14f9e4b26cbc14ea63db",
+    "url": "/Postuj/index.html"
+  },
+  {
+    "revision": "be1c619d44420e28bad2",
+    "url": "/Postuj/static/css/main.f75d526b.chunk.css"
+  },
+  {
+    "revision": "3738cba3e226669b9b68",
+    "url": "/Postuj/static/js/2.bd2b46e4.chunk.js"
+  },
+  {
+    "revision": "f5636e0105178ec9d0cd816877c59797",
+    "url": "/Postuj/static/js/2.bd2b46e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1c619d44420e28bad2",
+    "url": "/Postuj/static/js/main.0a4a4e1e.chunk.js"
+  },
+  {
+    "revision": "e088b0e2bfe96a222739",
+    "url": "/Postuj/static/js/runtime-main.f7d97643.js"
+  },
+  {
+    "revision": "60f0926cc9035d2e9b420ccff93e8b60",
+    "url": "/Postuj/static/media/add_post_form_camera.60f0926c.svg"
+  },
+  {
+    "revision": "5bb294bac7185c4c103fc1938c4e1416",
+    "url": "/Postuj/static/media/observed_popup-icon_close.5bb294ba.svg"
+  },
+  {
+    "revision": "042aa417f110588538aefef728ed90fa",
+    "url": "/Postuj/static/media/post-icon_add_to_favorite.042aa417.svg"
+  },
+  {
+    "revision": "4c67d8751e56b25704ac9b791835bef8",
+    "url": "/Postuj/static/media/post-icon_favorite.4c67d875.svg"
+  },
+  {
+    "revision": "ca3efbf7e05bf999cf240b6cc2b75305",
+    "url": "/Postuj/static/media/post-icon_like.ca3efbf7.svg"
+  },
+  {
+    "revision": "1ad14ac7bd53e261182107de4e33255b",
+    "url": "/Postuj/static/media/theme_dark.1ad14ac7.svg"
+  },
+  {
+    "revision": "16c410307caeb1f78fd7fe4da2dcb99c",
+    "url": "/Postuj/static/media/theme_light.16c41030.svg"
+  },
+  {
+    "revision": "4582a3681f06bac1a88a44967e74ed1a",
+    "url": "/Postuj/static/media/theme_mood.4582a368.svg"
+  }
+]);
